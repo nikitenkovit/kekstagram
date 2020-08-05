fb7ce66a3fe465643e61a7fe92bb62c8ec82b978
Vitaliy <nelot@mail.ru>
2020-08-05 23:11:46 +0300

    add pictures.js and package.js

---
+++ js/package.js
@@ -0,0 +1,154 @@
+export let objectArray = [
+  {
+    "url": 'photos/1.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Отдыхаем...'
+  },
+  {
+    "url": 'photos/2.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Затусили с друзьями на море'
+  },
+  {
+    "url": 'photos/3.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Затусили с друзьями на море'
+  },
+  {
+    "url": 'photos/4.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Тестим новую камеру!'
+  },
+  {
+    "url": 'photos/5.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Как же круто тут кормят'
+  },
+  {
+    "url": 'photos/6.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Вот это тачка!'
+  },
+  {
+    "url": 'photos/7.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Как же круто тут кормят'
+  },
+  {
+    "url": 'photos/8.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Тестим новую камеру!'
+  },
+  {
+    "url": 'photos/9.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Затусили с друзьями на море'
+  },
+  {
+    "url": 'photos/10.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......'
+  },
+  {
+    "url": 'photos/11.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Отдыхаем...'
+  },
+  {
+    "url": 'photos/12.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Вот это тачка!'
+  },
+  {
+    "url": 'photos/13.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Как же круто тут кормят'
+  },
+  {
+    "url": 'photos/14.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Тестим новую камеру!'
+  },
+  {
+    "url": 'photos/15.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Отдыхаем...'
+  },
+  {
+    "url": 'photos/16.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......'
+  },
+  {
+    "url": 'photos/17.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Отдыхаем...'
+  },
+  {
+    "url": 'photos/18.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Тестим новую камеру!'
+  },
+  {
+    "url": 'photos/19.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Тестим новую камеру!'
+  },
+  {
+    "url": 'photos/20.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Затусили с друзьями на море'
+  },
+  {
+    "url": 'photos/21.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Как же круто тут кормят'
+  },
+  {
+    "url": 'photos/22.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Отдыхаем...'
+  },
+  {
+    "url": 'photos/23.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......'
+  },
+  {
+    "url": 'photos/24.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Отдыхаем...'
+  },
+  {
+    "url": 'photos/25.jpg',
+    "likes": 0,
+    "comments": "",
+    "description": 'Как же круто тут кормят'
+  }
+]
+
+export let commentsArray = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
---
+++ js/pictures.js
@@ -0,0 +1,127 @@
+/*creating elements from a template*/
+
+import {objectArray} from './package.js';
+import {commentsArray} from './package.js';
+
+const getRandomNumber = (min, max) => (Math.ceil(Math.random() * (max - min) + min));
+const getRandomRoundNumber = (min, max) => (Math.round(Math.random() * (max - min) + min));
+
+const templateContent = document.getElementById('picture').content;
+const templateElement = templateContent.querySelector('.picture__link');
+
+const createNewElement = element => {
+
+  const clonedElement = templateElement.cloneNode(true);
+  const newImg = clonedElement.querySelector('.picture__img');
+  const newLikesNumber = clonedElement.querySelector('.picture__stat--likes');
+  const newCommentsNumber = clonedElement.querySelector('.picture__stat--comments')
+
+  newImg.src = element.url;
+  newLikesNumber.textContent = getRandomNumber(15, 200);
+  newCommentsNumber.textContent = getRandomNumber(0, 2);
+  clonedElement.setAttribute("data-description", element.description);
+
+  return clonedElement;
+};
+
+/*add elements on page*/
+
+const picturesContainer = document.querySelector('.pictures');
+
+const addElementsOnPage = () => {
+  let fragment = document.createDocumentFragment();
+
+  objectArray.forEach(element => {
+    fragment.appendChild(createNewElement(element));
+  });
+  picturesContainer.appendChild(fragment);
+};
+
+addElementsOnPage();
+
+/*show big picture*/
+
+const bigPicture = document.querySelector('.big-picture');
+const allPictureLink = document.querySelectorAll('.picture__link');
+
+const addPictureLinkHandler = element => {
+  element.onclick = evt => {
+    evt.preventDefault();
+    createBigPicture(element);
+  };
+};
+
+allPictureLink.forEach(element => addPictureLinkHandler(element));
+
+const createBigPicture = element => {
+
+  bigPicture.classList.remove('hidden');
+
+  const elementImg = element.querySelector('.picture__img');
+  const bigPictureImg = document.querySelector('.big-picture__img--img');
+  const elementStatLikes = element.querySelector('.picture__stat--likes')
+  const likesCount = document.querySelector('.likes-count');
+  const elementStatComments = element.querySelector('.picture__stat--comments');
+  const commentsCount = document.querySelector('.comments-count');
+  const description = document.querySelector('.social__caption');
+
+  bigPictureImg.src = elementImg.src;
+  likesCount.textContent = elementStatLikes.textContent;
+  commentsCount.textContent = elementStatComments.textContent;
+  createComments(parseInt(elementStatComments.textContent, 10));
+  description.textContent = element.dataset.description;
+};
+
+const createComments = numberOfComments => {
+  let commentsList = document.querySelector('.social__comments');
+
+  commentsList.innerHTML = "";
+
+  for (let i = 0; i < numberOfComments; i++) {
+    let li = document.createElement('li');
+    li.classList.add('social__comment');
+    li.innerHTML = '<img class="social__picture"  alt="Аватар комментатора фотографии" width="35" height="35" src="">' +
+      '<p class="social__text"></p>';
+
+    commentsList.appendChild(li);
+
+    const allAvatars = commentsList.querySelectorAll('.social__picture');
+    allAvatars[i].src = 'img/avatar-' + getRandomNumber(1, 6) + '.svg'
+    const allTextComments = commentsList.querySelectorAll('.social__text');
+    allTextComments[i].textContent = commentsArray[getRandomRoundNumber(0, 6)];
+  }
+};
+
+/*hidden big picture*/
+
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+closeButton.onclick = () => bigPicture.classList.add('hidden');
+
+window.onkeydown = evt => {
+  if (evt.keyCode === 27) bigPicture.classList.add('hidden');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
